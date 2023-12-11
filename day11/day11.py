abc21ed872a4ba217844170333d3fbaff3bec424
Gordin508 <5062634+Gordin508@users.noreply.github.com>
2023-12-11 12:17:53 +0100

    Add solution for day11 part 2

--- day11/day11.py
+++ day11/day11.py
@@ -24,6 +24,7 @@ class Universe:
                         logger.error(f"Unexpected at {(y, x)}: {char}")
         self.updateGalaxyMap()
         logger.info(f"Parsed {y} x {x} grid with {self.getNumGalaxies()} galaxies")
+        self.age = 1
 
     def getNumGalaxies(self):
         return len(self.galaxymap)
@@ -37,7 +38,27 @@ class Universe:
         self.reverseGalaxymap = {val: key for (key, val) in self.galaxymap.items()}
         assert np.sum(self.grid) == len(self.galaxymap) == len(self.reverseGalaxymap)
 
+    def setVirtualAge(self, age):
+        self.age = age
+        self.emptyrows = [y for y in range(self.height) if np.sum(self.grid[y]) == 0]
+        self.emptycolumns = [x for x in range(self.width) if np.sum(self.grid[:,x]) == 0]
+
+    def getDistance(self, galaxy1: int, galaxy2: int):
+        g1pos = self.galaxymap[galaxy1]
+        g2pos = self.galaxymap[galaxy2]
+        rawdist = abs(g1pos[0] - g2pos[0]) + abs(g1pos[1] - g2pos[1])
+
+        def minmax(pos1, pos2, axis):
+            return (min(pos1[axis], pos2[axis]), max(pos1[axis], pos2[axis]))
+        # how many expanding rows and columns are we crossing?
+        y1, y2 = minmax(g1pos, g2pos, 0)
+        x1, x2 = minmax(g1pos, g2pos, 1)
+        emptyrows = len([row for row in self.emptyrows if row > y1 and row < y2])
+        emptycolumns = len([col for col in self.emptycolumns if col > x1 and col < x2])
+        return rawdist + (emptycolumns + emptyrows) * self.age
+
     def expand(self):
+        # use only for part1!
         emptyrows = [y for y in range(self.height) if np.sum(self.grid[y]) == 0]
         emptycolumns = [x for x in range(self.width) if np.sum(self.grid[:,x]) == 0]
         if len(emptyrows) != 0:
@@ -88,27 +109,35 @@ class Universe:
     def __repr__(self):
         return self.__str__()
 
-    def shortestPaths(self):
+    def shortestPaths(self, usevirtual=False):
         # pairwise shortest paths
         pathlengths = []
         numgalaxies = self.getNumGalaxies()
-        for g1, g2 in ((g1, g2) for g1 in range(0, numgalaxies) for g2 in range(g1 + 1, numgalaxies)):
-            g1pos = self.galaxymap[g1]
-            g2pos = self.galaxymap[g2]
-            pathlengths.append(abs(g1pos[0] - g2pos[0]) + abs(g1pos[1] - g2pos[1]))
-            logger.debug(f"{(g1, g2)}: distance {pathlengths[-1]}")
+        if not usevirtual:
+            for g1, g2 in ((g1, g2) for g1 in range(0, numgalaxies) for g2 in range(g1 + 1, numgalaxies)):
+                g1pos = self.galaxymap[g1]
+                g2pos = self.galaxymap[g2]
+                pathlengths.append(abs(g1pos[0] - g2pos[0]) + abs(g1pos[1] - g2pos[1]))
+                logger.debug(f"{(g1, g2)}: distance {pathlengths[-1]}")
+        else:
+            for g1, g2 in ((g1, g2) for g1 in range(0, numgalaxies) for g2 in range(g1 + 1, numgalaxies)):
+                pathlengths.append(self.getDistance(g1, g2))
+
         return sum(pathlengths)
 
 
 def part1(lines):
     universe = Universe(lines)
-    universe.expand()
+    universe.setVirtualAge(1)
     # print(universe)
-    print(f"Part 1: {universe.shortestPaths()}")
+    print(f"Part 1: {universe.shortestPaths(usevirtual=True)}")
 
 
 def part2(lines):
-    pass
+    universe = Universe(lines)
+    universe.setVirtualAge(int(1e6) - 1)
+    # print(universe)
+    print(f"Part 2: {universe.shortestPaths(usevirtual=True)}")
 
 
 def main():
