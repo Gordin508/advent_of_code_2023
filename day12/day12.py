79d0fd48340685179412c2d8ee97422db735008a
Gordin508 <5062634+Gordin508@users.noreply.github.com>
2023-12-15 15:42:07 +0100

    Integrate dynamic programming solution with rest of code

--- day12/day12.py
+++ day12/day12.py
@@ -29,9 +29,12 @@ class Springs:
     def __init__(self, line: str, isFold=False):
         split = line.strip().split(" ", maxsplit=1)
         self.rawconditions = split[0]
-        self.conditions = [Condition.from_char(x) for x in split[0]]
+        if isFold:
+            self.rawconditions = "?".join(self.rawconditions for _ in range(5))
+        self.conditions = [Condition.from_char(x) for x in self.rawconditions]
         self.record = [int(x.strip()) for x in split[1].split(",")]
-        self.isFold = isFold
+        if isFold:
+            self.record = [x for _ in range(5) for x in self.record]
 
     def __str__(self):
         return f"{self.rawconditions} {self.record}"
@@ -39,7 +42,48 @@ class Springs:
     def __repr__(self):
         return self.__str__()
 
-    def numPossibilities(self):
+    def possibilities(self):
+        # dynamic programming
+        from functools import cache
+
+        @cache
+        def possfunc(numindices, numrecords):
+            assert len(self.record) >= numrecords >= 0
+            assert len(self.conditions) >= numindices >= 0
+
+            if numindices == 0 and numrecords == 0:
+                # base case
+                return 1
+
+            if numrecords == 0:
+                # no more spring may be broken
+                return 1 if all(condition != Condition.BROKEN for condition in self.conditions[:numindices]) else 0
+
+            currentstreak = self.record[numrecords - 1]
+
+            if currentstreak > numindices:
+                # demanded number is higher than number of springs left
+                return 0
+
+            if self.conditions[numindices - 1] == Condition.OPERATIONAL:
+                # fast forward, we do not care for operational wells
+                return possfunc(numindices - 1, numrecords)
+
+            result = 0
+            if all(condition != Condition.OPERATIONAL for condition
+                   in (self.conditions[i] for i in range(numindices - currentstreak, numindices))):
+                if currentstreak == numindices or self.conditions[numindices - currentstreak - 1] != Condition.BROKEN:
+                    # streak is not too short and not too long, so we can pop of the last record
+                    result += possfunc(max(0, numindices - currentstreak - 1), numrecords - 1)
+            if self.conditions[numindices - 1] == Condition.UNKNOWN:
+                # we have a question mark at the current pos
+                # we also consider the possibilties where we set it to operational
+                result += possfunc(numindices - 1, numrecords)
+            return result
+
+        return possfunc(len(self.conditions), len(self.record))
+
+    def possibilities_bruteforce(self):
         def getUnknowns(conditions):
              return [i for (i, cond) in enumerate(conditions) if cond == Condition.UNKNOWN]
 
@@ -93,24 +137,18 @@ def part1(lines):
     result = 0
     for line in lines:
         spring = Springs(line)
-        possibilities = spring.numPossibilities()
-        result += possibilities
+        result += spring.possibilities()
 
     logger.info(f"Part 1: {result}")
 
 
 def part2(lines):
-    import ipdb; ipdb.set_trace()
     result = 0
 
+    import ipdb; ipdb.set_trace()
     for line in lines:
-        spring = Springs(line)
-        possibilities = spring.numPossibilities()
-        spring2 = Springs("?" + line, isFold=True)
-        possibilities2 = spring2.numPossibilities()
-        tmp = possibilities * (possibilities2**4)
-        logger.info(f"{possibilities} {possibilities2}: {tmp}")
-        result += tmp
+        spring = Springs(line, isFold=True)
+        result += spring.possibilities()
 
     logger.info(f"Part 2: {result}")
 
@@ -119,7 +157,7 @@ def main():
     infile = sys.argv[1] if len(sys.argv) > 1 else 'input.txt'
     with open(infile) as f:
         lines = f.readlines()
-    # part1(lines)
+    part1(lines)
     part2(lines)
 
 
